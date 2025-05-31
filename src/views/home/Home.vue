0e50d00c29de253143afe36ce1185e9258ea637b
Rakku <rakku2code@gmail.com>
2025-05-31 09:56:19 +0800

    feat: setup home page, navigation routing, main sections, logo and favicon

---
+++ src/views/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page">
+    <h1>歡迎來到 JoinBar</h1>
+    <p>這裡是最懂喝的聚會平台！</p>
+  </div>
+
+</template>
+
+<style scoped>
+.page {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
---
+++ src/views/sub/Subscription.vue
@@ -0,0 +1,12 @@
+<template>Add commentMore actions
+  <div class="page">
+    <h1>訂閱優惠</h1>
+    <p>訂閱最新優惠活動與消息。</p>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  padding: 2rem;
+}
+</style>
