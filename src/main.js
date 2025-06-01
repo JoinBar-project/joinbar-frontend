be3172776ef80ad8c0b5b890b122931295fa9309
Ben <ab4827qq@gmail.com>
2025-06-01 23:14:11 +0800

    refactor: migrate cartStore to use Pinia persist plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.css'
 
+const pinia = createPinia().use(piniaPluginPersistedstate)
+
 createApp(App)
-  .use(createPinia())
+  .use(pinia)
   .use(router)
   .mount('#app')
