8a2d1ae7e2346b69215329e5ee8c87a607519ab1
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-06-03 14:44:46 +0800

    feat : add message board page HTML

---
+++ src/components/events/MessageBoard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="event-message-section">
+    <div class="event-message-bg">
+      <div class="message-board-title">BarTalks 留言板</div>
+      <div class="message-board">
+        <div class="message-content">
+          <div class="headshot">
+            <img src="../../public/大頭照1.jpg" alt="大頭照">
+          </div>
+          <div class="message">
+            <p class="user">新店裕隆成</p>
+            <p>這裡看起來很讚!!</p>
+            <p class="message-date">2025/06/01 20:54</p>
+          </div>
+        </div>
+        <div class="message-content">
+          <div class="headshot">
+            <img src="../../public/大頭照2.jpg" alt="大頭照">
+          </div>
+          <div class="message">
+            <p class="user">台北蔡一零</p>
+            <p>那天我要睡在路邊! 喝到爛醉~~</p>
+            <p class="message-date">2025/06/01 20:54</p>
+          </div>
+        </div>
+        <div class="message-area">
+          <textarea class="textarea" placeholder="來分享你的想法吧！">
+          </textarea>
+          <div class="messgae-tool">
+            <i class="fa-regular fa-face-smile"></i>
+            <button class="messgae-btn" type="button">送出</button>
+          </div>
+          <div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
