e53449a1e1e0dad93aeb84a2097af2b9ec0ed3b7
Ben <ab4827qq@gmail.com>
2025-06-03 12:58:41 +0800

    refactor: move cart icon from homepage to member page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,6 @@
       <li><RouterLink to="/events">酒吧活動</RouterLink></li>
       <li><RouterLink to="/subscription">訂閱優惠</RouterLink></li>
       <li><RouterLink to="/member">會員服務</RouterLink></li>
-      <li><RouterLink to="/cart"><img class="cart-icon" src="/cart.png" alt="Cart Icon" /></RouterLink></li>
     </ul>
   </nav>
 </template>
@@ -52,11 +51,5 @@
   display: flex;
   align-items: center;
   height: 100%;
-}
-
-.cart-icon {
-  height: 55%;
-  width: auto;
-  display: block;
 }
 </style>
