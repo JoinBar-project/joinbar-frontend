2bce1cd20f953b13e51f2d5f1e544e8b19d25def
Ben <ab4827qq@gmail.com>
2025-06-03 13:24:44 +0800

    refactor: move cart icon from member page to navbar

--- src/views/member/Member.vue
+++ src/views/member/Member.vue
@@ -1,33 +1,12 @@
 <template>
   <div class="page">
-    <img src="/cart.png" alt="Cart Icon" class="cart-icon" @click="goCart">
     <h1>會員服務</h1>
     <p>管理您的帳號與偏好設定。</p>
   </div>
 </template>
 
-<script setup>
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const goCart = () => {
-  router.push('/cart')
-}
-</script>
-
 <style scoped>
 .page {
-  padding: 32px;
-  position: relative;
-}
-
-.cart-icon {
-  position: absolute;
-  top: 16px;
-  right: 16px;
-  width: 32px;
-  height: 32px;
-  cursor: pointer;
+  padding: 2rem;
 }
 </style>
