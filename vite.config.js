c5b843963a88aeca994b9a9086992556dbefefcc
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-06-04 00:53:04 +0800

    feat :  remove repeat import tailwind

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path';
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
