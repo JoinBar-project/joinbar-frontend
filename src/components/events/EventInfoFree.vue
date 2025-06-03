017ea7c02ab373d749586d7ac970f45e18caaa4f
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-06-03 14:27:45 +0800

    feat : add event for free page

---
+++ src/components/events/EventInfoFree.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="event-information-section">
+    <div class="event-information-card">
+      <div class="event-img">
+        <img src="../../../public/酒吧示意圖.jpg" alt=""> 
+      </div>
+      <div class="event-content-box">
+        <div class="event-map">
+        </div>
+        <div class="event-content">
+          <div class="event-tags">
+            <div>免費活動</div>
+            <div>下班來喝</div>
+          </div>
+          <div>
+            <h3 class="event-title">周末CHILL調酒Day，藍調爵士抒情夜
+              入場免費招待一杯SHOT，歡迎加入!
+            </h3>
+            <div class="event-content-info">
+              <i class="fa-solid fa-calendar"></i>
+              <p>2025.01.01(一) 21:00 ~ 2025.02.28(五) 23:59</p>
+            </div>
+            <div class="event-content-info">
+              <i class="fa-solid fa-location-dot"></i>
+              <p>BAR AMIGO｜新北市板橋區中正路100號</p>
+            </div>
+            <div class="event-content-info">
+              <i class="fa-solid fa-user"></i>
+              <p>目前報名人數： <span>12</span> / <span>20</span></p>
+            </div>
+          </div>
+          <button type="button" class="event-btn event-btn-free">免費參加</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
