93338d4d23e5ccae2043a43493972b9c32ed6556
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-06-03 14:26:15 +0800

    feat: create event organizer HTML and CSS, fix @ path alias issue

--- src/views/events/Event.vue
+++ src/views/events/Event.vue
@@ -1,12 +1,28 @@
+<script setup>
+// import FormModal from '@/components/events/FormModal.vue'
+import EventHoster from '@/components/events/EventHoster.vue'
+
+</script>
+
 <template>
+
   <div class="page">
     <h1>酒吧活動</h1>
     <p>參加近期活動，結識朋友！</p>
+    <p>探索附近的酒吧活動。</p>
   </div>
+
+  <!-- <FormModal /> -->
+  <EventHoster />
+
 </template>
+
 
 <style scoped>
 .page {
   padding: 2rem;
+  text-align: center;
+  background-color: var(--color-black);
+  color: white;
 }
 </style>
