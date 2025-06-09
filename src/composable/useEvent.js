6845ab4571733c13dfab36dad1b50a932b939cc6
Bella-Cheng <dhp951320@gmail.com>
2025-06-10 00:02:17 +0800

    feat : feat: implement shared logic for registration process

--- src/composable/useEvent.js
+++ src/composable/useEvent.js
@@ -1,29 +1,47 @@
 import { ref, computed } from 'vue';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import 'dayjs/locale/zh-tw'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 dayjs.locale('zh-tw')
 
-export function useEvent(){
+const tz = 'Asia/Taipei'
+const joinedNum = ref(0)
+
+export function useEvent(event){
   
   const isJoin = ref(false)
   const showModal = ref(false)
   const now = ref(dayjs())
 
   const isOver24hr = computed(() => {
-    if( !event.value || !event.value.startDate ) return false
-    return dayjs(event.value.startDate).diff(now.value, 'hour') > 24
+    if( !event.value?.startDate ) return null
+    return dayjs(event.value.startDate).tz(tz).diff(now.value, 'hour') > 24
   })
 
   const formattedEventTime = computed(() =>{
     if (!event.value) return ''
-    const start = dayjs(event.value.startDate).format('YYYY.MM.DD HH:mm (ddd)')
-    const end = dayjs(event.value.endDate).format('YYYY.MM.DD HH:mm (ddd)')
-    return `${start} ~ ${end}`
+    const start = event.value?.startDate
+    const end = event.value?.endDate
+
+    if (!start || !end) return ''
+
+    const formattedStart =  dayjs(event.value.startDate).tz(tz).format('YYYY.MM.DD HH:mm (ddd)')
+    const formattedEnd =  dayjs(event.value.endDate).tz(tz).format('YYYY.MM.DD HH:mm (ddd)')
+    return `${formattedStart} ~ ${formattedEnd}`
   })
 
   function toggleJoin(){
     isJoin.value = !isJoin.value
-    console.log('現在狀態是：', isJoin.value)
+
+    if( isJoin.value == true){
+      joinedNum.value++
+    }else{
+      joinedNum.value--
+    }
   }
 
   function openCancelModal(){
@@ -35,12 +53,13 @@ export function useEvent(){
   }
 
   function handleConfirmCancel(){
-    isJoin.value = false
+    toggleJoin()
     showModal.value = false
   }
 
   return{
     isJoin,
+    joinedNum,
     toggleJoin,
     isOver24hr,
     formattedEventTime,
