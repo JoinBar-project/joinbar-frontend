e4f88fbda17c74aed4fe9959133db87be7c6da7f
2xin15 <s111291519xin@gapp.nthu.edu.tw>
2025-06-09 18:19:16 +0800

    chore: update import path and route config for Login view

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home/Home.vue'
 import Event from '@/views/events/Event.vue'
 import MapView from '@/views/map/MapView.vue'
-import Member from '@/views/member/Member.vue'
+import Member from '@/views/member/auth/Member.vue'
 import Reviews from '@/views/reviews/Reviews.vue'
 import Subscription from '@/views/sub/Subscription.vue'
 import Cart from '@/views/cart/Cart.vue'
-import Payment from "@/views/cart/Payment.vue";
+import Payment from "@/views/cart/Payment.vue"
+import Login from "@/views/member/auth/Login.vue";
 
 const routes = [
   { path: '/home', name: 'Home', component: Home },
+  { path: '/login', name: 'Login', component: Login },
   { path: '/events', name: 'Event', component: Event },
   { path: '/map', name: 'Map', component: MapView },
   { path: '/member', name: 'Member', component: Member },
