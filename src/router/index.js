887a1dd05d3613b41849d98505235874919ed95c
2xin15 <s111291519xin@gapp.nthu.edu.tw>
2025-06-10 15:21:11 +0800

    fix: delete tel form login

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,12 @@ import Reviews from '@/views/reviews/Reviews.vue'
 import Subscription from '@/views/sub/Subscription.vue'
 import Cart from '@/views/cart/Cart.vue'
 import Payment from "@/views/cart/Payment.vue"
-import Login from "@/views/member/auth/Login.vue";
+import Login from "@/views/member/auth/Login.vue"
+// import Register from '@/views/member/auth/Register.vue';
 
 const routes = [
   { path: '/home', name: 'Home', component: Home },
+  // { path: '/register', name: 'register', component: Register },
   { path: '/login', name: 'Login', component: Login },
   { path: '/events', name: 'Event', component: Event },
   { path: '/map', name: 'Map', component: MapView },
