d4fbd4a234cc57b29f21817212f0e4d964093457
Rakku <rakku2code@gmail.com>
2025-06-13 02:38:27 +0800

    style: clean up code formatting and remove comments

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     tailwindcss()
   ],
   resolve: {
