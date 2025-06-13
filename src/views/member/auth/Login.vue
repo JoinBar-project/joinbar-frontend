e4ed71a179132ed4b7e87bf2bad9e46a7ec759bc
Rean Tsai <aobc55625@gmail.com>
2025-06-13 19:28:14 +0800

    refactor: move auth store to api directory and update import paths

--- src/views/member/auth/Login.vue
+++ src/views/member/auth/Login.vue
@@ -96,7 +96,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { useAuthStore } from '@/stores/auth'
+import { useAuthStore } from '@/api/auth'
 import Swal from 'sweetalert2';
 
 const authStore = useAuthStore()
