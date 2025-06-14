639dc8671072f11d35178e9759f3d58a53030328
Bella-Cheng <dhp951320@gmail.com>
2025-06-14 23:56:33 +0800

    feat : add subscription template and tailwind

---
+++ src/components/sub/Subscription.vue
@@ -0,0 +1,140 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="max-w-screen">
+      <div class="bg-[url('@/components/sub/picture/bar-background2.jpg')] py-20 w-full relative bg-cover bg-[center_88%] opacity-92">
+        <div class="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0.1)_20%,_rgba(0,0,0,0.9)_100%)]"></div>
+        <div class="mb-4 relative z-20">
+          <h2 class="title text-center p-10 text-7xl text-cente text-stone-50 font-bold">酒友卡訂閱</h2>
+          <p class="title text-center text-stone-50 text-2xl">每月酒香，讓友情更醇厚</p>
+        </div>
+        
+      </div>
+      <div class="max-w-7xl m-auto">
+        <div class="grid grid-cols-3 gap-20" >
+          <div class="bg-[var(--color-black)] flex border rounded-[16px] my-20 hover:scale-105 transition-transform duration-300">
+            <div class="m-auto w-[66%]">
+              <h3 class="text-6xl text-stone-50 pt-10 pb-4 w-full">尊爵黑卡
+              </h3>
+              <p class="text-zinc-300 py-10 text-center">
+                <span class="text-[var(--color-primary-orange)] text-3xl">$3,000
+                </span> / 365 天
+              </p>
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  VIP 專屬特調 3 次 / 年
+                </p>
+              </div>
+            
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  合作酒吧招待飲品 6 次 / 年
+                </p>
+              </div>
+
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  合作酒吧招待小點 6 次 / 年
+                </p>
+              </div>
+
+              <button 
+                type="button" 
+                class="mt-20 mb-10 px-6 py-2 text-lg text-stone-50 border-2 border-[var(--color-primary-orange)] rounded-[12px] bg-neutral-900 block mx-auto cursor-pointer hover:bg-[var(--color-primary-orange)]">即刻擁有
+              </button>
+            </div>
+          </div>
+
+          <div class="bg-[var(--color-black)] flex border rounded-[16px] my-20 hover:scale-105 transition-transform duration-300">
+            <div class="m-auto w-[66%]">
+              <h3 class="text-6xl text-stone-50 pt-10 pb-4 w-full">季訂方案
+              </h3>
+              <p class="text-zinc-300 py-10 text-center">
+                <span class="text-[var(--color-primary-orange)] text-3xl">$2,000
+                </span> / 90 天
+              </p>
+
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  VIP 專屬特調 2 次 / 年
+                </p>
+              </div>
+            
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  合作酒吧招待飲品 3 次 / 季
+                </p>
+              </div>
+
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  合作酒吧招待小點 3 次 / 季
+                </p>
+              </div>
+
+              <button 
+                type="button" 
+                class="mt-20 mb-10 px-6 py-2 text-lg text-stone-50 border-2 border-[var(--color-primary-orange)] rounded-[12px] bg-neutral-900 block mx-auto cursor-pointer hover:bg-[var(--color-primary-orange)]">即刻擁有
+              </button>
+            </div>
+          </div>
+
+          <div class="bg-[var(--color-black)] flex border rounded-[16px] my-20 hover:scale-105 transition-transform duration-300">
+            <div class="m-auto w-[66%]">
+              <h3 class="text-6xl text-stone-50 pt-10 pb-4 w-full">小資月卡
+              </h3>
+              <p class="text-zinc-300 py-10 text-center">
+                <span class="text-[var(--color-primary-orange)] text-3xl">$1,000
+                </span> / 30 天
+              </p>
+
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  VIP 專屬特調 1 次 / 年
+                </p>
+              </div>
+            
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  合作酒吧招待飲品 1 次 / 月
+                </p>
+              </div>
+
+              <div class="flex py-4">
+                <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
+                <p class=" text-stone-50 font-bold">
+                  合作酒吧招待小點 1 次 / 月
+                </p>
+              </div>
+
+              <button 
+                type="button" 
+                class="mt-20 mb-10 px-6 py-2 text-lg text-stone-50 border-2 border-[var(--color-primary-orange)] rounded-[12px] bg-neutral-900 block mx-auto cursor-pointer hover:bg-[var(--color-primary-orange)]">即刻擁有
+              </button>
+            </div>
+          </div> 
+        </div>
+      </div>
+
+    </div>
+  </div>
+    
+</template>
+
+<style scoped>
+
+.title{
+  text-shadow: 2px 2px 8px rgba(0,0,0,2);
+}
+
+</style>
--- src/views/map/MapView.vue
+++ src/views/map/MapView.vue
@@ -1,4 +1,5 @@
-<template>
+<template></template>
+<!-- <template>
   <div class="map-view-container">
     <div class="top-left-controls">
       <button
@@ -950,4 +951,4 @@ watch(selectedBar, (newVal) => {
     width: 100%;
   }
 }
-</style>
+</style> -->
