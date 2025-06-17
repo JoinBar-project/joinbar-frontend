e64f0e0bbdcea28728805d442f026198364c1458
2xin15 <s111291519xin@gapp.nthu.edu.tw>
2025-06-17 17:02:58 +0800

    feat: manage user favorite bar information

--- src/stores/wishlist.js
+++ src/stores/wishlist.js
@@ -1,31 +1,19 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useWishlistStore = defineStore('wishlist', {
-  state: () => ({
-    favorites: []
-  }),
-  actions: {
-    add(bar) {
-      if (!this.favorites.find(item => item.id === bar.id)) {
-        this.favorites.push(bar)
-      }
-    },
-    remove(barId) {
-      this.favorites = this.favorites.filter(item => item.id !== barId)
-    },
-    toggle(bar) {
-      const index = this.favorites.findIndex(item => item.id === bar.id)
-      if (index === -1) {
-        this.favorites.push(bar)
-      } else {
-        this.favorites.splice(index, 1)
-      }
-    }
-  },
-  getters: {
-    isFavorite: (state) => (barId) => {
-      return state.favorites.some(bar => bar.id === barId)
+// Composition API 形式
+export const useWishlistStore = defineStore('wishlist', () => {
+  const wishlist = ref([])
+  // 切換收藏狀態
+  const toggle = (bar) => {
+    const index = wishlist.value.findIndex(b => b.id === bar.id || b.place_id === bar.place_id)
+    if (index !== -1) {
+      wishlist.value.splice(index, 1)
+    } else {
+      wishlist.value.push({ ...bar, isWishlisted: true })
     }
   }
+
+  return { wishlist, toggle }
 })
 
