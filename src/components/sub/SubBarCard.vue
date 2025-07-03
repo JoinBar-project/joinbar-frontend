1a6095e6cf0bcc14c88c49f712c14b9ad90b6d29
Bella-Cheng <dhp951320@gmail.com>
2025-07-03 20:13:13 +0800

    feat : add RWD

--- src/components/sub/SubBarCard.vue
+++ src/components/sub/SubBarCard.vue
@@ -1,13 +1,17 @@
 <script setup>
 
-import { ref, computed } from 'vue'
+import {  ref, computed, onMounted, onUnmounted } from 'vue'
 
 const currentIndex = ref(0)
-const visibleCount = 3
+const visibleCount = ref(3)
+
+const updateVisibleCount = () => {
+  visibleCount.value = window.innerWidth < 768 ? 1 : 3
+}
 
 const visibleBars = computed(() => {
   const bars = []
-  for (let i = 0; i < visibleCount; i++) {
+  for (let i = 0; i < visibleCount.value; i++) {
     const index = (currentIndex.value + i) % barList.value.length
     bars.push(barList.value[index])
   }
@@ -15,64 +19,47 @@ const visibleBars = computed(() => {
 })
 
 function prev() {
-  
   currentIndex.value =
-    (currentIndex.value - 1 + barList.value.length) % barList.value.length
+    (currentIndex.value - visibleCount.value + barList.value.length) % barList.value.length
 }
 
 function next() {
-  currentIndex.value = (currentIndex.value + 1) % barList.value.length
+  currentIndex.value = (currentIndex.value + visibleCount.value) % barList.value.length
 }
 
-
-
 const barList = ref([
-  { 
-    id: 1, 
-    name: '醉後不歸路', 
-    location: '台北市大安區信義路123號', 
-    image: new URL('../../assets/sub/sub-bar1.jpg', import.meta.url).href 
-  },
-  { id: 2, 
-    name: '夜色迷醉', 
-    location: '台北市中山區林森北路88號', 
-    image: new URL('../../assets/sub/sub-bar2.jpg', import.meta.url).href 
-  },
-  { id: 3, 
-    name: '微醺時刻', 
-    location: '新北市板橋區文化路二段101號', 
-    image: new URL('../../assets/sub/sub-bar3.jpg', import.meta.url).href
-  },
-  { 
-    id: 4, 
-    name: '霓虹之夜', 
-    location: '台中市西區公益路150號', 
-    image: new URL('../../assets/sub/sub-bar4.jpg', import.meta.url).href
-  },
-  { 
-    id: 5, 
-    name: '小酌時光', 
-    location: '高雄市鼓山區美術東五路3號', 
-    image: new URL('../../assets/sub/sub-bar5.jpg', import.meta.url).href
-  },
+  { id: 1, name: '醉後不歸路', location: '台北市大安區信義路123號', image: new URL('../../assets/sub/sub-bar1.jpg', import.meta.url).href },
+  { id: 2, name: '夜色迷醉', location: '台北市中山區林森北路88號', image: new URL('../../assets/sub/sub-bar2.jpg', import.meta.url).href },
+  { id: 3, name: '微醺時刻', location: '新北市板橋區文化路二段101號', image: new URL('../../assets/sub/sub-bar3.jpg', import.meta.url).href },
+  { id: 4, name: '霓虹之夜', location: '台中市西區公益路150號', image: new URL('../../assets/sub/sub-bar4.jpg', import.meta.url).href },
+  { id: 5, name: '小酌時光', location: '高雄市鼓山區美術東五路3號', image: new URL('../../assets/sub/sub-bar5.jpg', import.meta.url).href },
 ])
+
+onMounted(() => {
+  updateVisibleCount()
+  window.addEventListener('resize', updateVisibleCount)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateVisibleCount)
+})
 
 </script>
 
 
 <template >
-  <div class="relative max-w-7xl pb-40 mx-auto">
-    <div class="text-5xl text-center pb-20 font-bold">合作酒吧</div>
+  <div class="relative mx-6 md:max-w-7xl pb-10 md:pb-40 md:mx-auto">
+    <div class="text-4xl md:text-5xl text-center pb-8 md:pb-20 font-bold"> 合作酒吧</div>
     <div class="flex justify-around items-center pb-4">
-      <button @click="prev">
-        <i class="fa-solid fa-caret-left text-6xl hover:text-[var(--color-primary-orange)]"></i>
+      <button @click="prev" class="w-10 md:w-auto flex justify-center">
+        <i class="fa-solid fa-caret-left text-6xl active:text-[var(--color-primary-orange)] md:hover:text-[var(--color-primary-orange)]"></i>
       </button>
 
-      <div class="flex gap-20 transition-all">
+      <div class="flex flex-col md:flex-row gap-6 md:gap-20 transition-all items-center">
         <div
           v-for="bar in visibleBars"
           :key="bar.id"
-          class="bg-white shadow-md rounded-xl overflow-hidden w-64 flex-shrink-0"
+          class="bg-white shadow-md rounded-xl overflow-hidden w-54 md:w-64 flex-shrink-0"
         >
           <img :src="bar.image" alt="bar image" class="w-full h-40 object-cover" />
           <div class="p-4">
@@ -81,8 +68,8 @@ const barList = ref([
           </div>
         </div>
       </div>
-      <button @click="next">
-        <i class="fa-solid fa-caret-right text-6xl hover:text-[var(--color-primary-orange)]"></i>
+      <button @click="next" class="w-10 md:w-auto flex justify-center">
+        <i class="fa-solid fa-caret-right text-6xl active:text-[var(--color-primary-orange)] md:hover:text-[var(--color-primary-orange)]"></i>
       </button>
     </div>
   </div>
--- src/components/sub/SubsCard.vue
+++ src/components/sub/SubsCard.vue
@@ -116,39 +116,39 @@ onUnmounted(() => {
 
 
 <template>
-  <div class="relative overflow-hidde">
+  <div class="w-full max-w-full md:relative overflow-hidde ">
     <div 
       ref="spotlight"
-      class="pointer-events-none fixed top-0 left-0 w-32 h-32 rounded-full
+      md:class="pointer-events-none fixed top-0 left-0 w-32 h-32 rounded-full
              bg-yellow-400 opacity-50 blur-3xl z-50 transition-transform duration-75 mix-blend-screen">
     </div>
 
-    <div class="bg-[url('@/assets/sub/bar-background2.jpg')] py-70 w-full relative bg-cover bg-[center_98%] opacity-92">
+    <div class="bg-[url('@/assets/sub/bar-background.webp')] py-30 md:py-70 w-full relative bg-cover bg-[center_98%] opacity-92">
       <div class="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0.1)_20%,_rgba(0,0,0,0.9)_100%)]"></div>
-      <div class="mb-4 relative z-20 bottom-35">
-        <h2 class="title text-center p-10 text-7xl text-stone-50 font-bold">酒友卡訂閱</h2>
-        <p class="text-center text-stone-50 text-2xl">每月酒香，讓友情更醇厚</p>
+      <div class="mb-4 relative z-20 md:bottom-35">
+        <h2 class="title text-center text-4xl md:p-10 md:text-7xl text-stone-50 font-bold">酒友卡訂閱</h2>
+        <p class="text-center text-stone-50 text-base md:text-2xl">每月酒香，讓友情更醇厚</p>
       </div>
     </div>
 
-    <div class="max-w-7xl m-auto">
-      <div class="grid grid-cols-3 gap-20 relative bottom-65">
+    <div class="md:max-w-7xl m-auto">
+      <div class="grid grid-cols-1 mx-6 md:grid-cols-3 md:gap-20 md:relative md:bottom-65">
         <div
           v-for="card in cardData"
           :key="card.type"
-          class="bg-[#211d1e] flex flex-col rounded-[16px] hover:scale-105 transition-transform duration-300 overflow-hidden"
+          class="bg-[#211d1e] flex flex-col rounded-[16px] mt-8 hover:scale-105 transition-transform duration-300 overflow-hidden"
         >
         <div
           class="h-[6px] w-full bg-gradient-to-r from-[var(--color-secondary-green)] to-[var(--color-primary-orange)]"
         ></div>
           <div class="w-[66%] mx-auto">
-            <h3 class="text-2xl md:text-5xl font-black text-transparent bg-clip-text bg-gradient-to-r from-[var(--color-primary-orange)] to-[var(--color-secondary-green)] mb-2 tracking-wide pt-10 pb-4 text-center">{{ card.title }}</h3>
-            <p class="text-zinc-300 py-10 text-center">
-              <span class="text-[var(--color-primary-orange)] text-3xl font-bold">
+            <h3 class="text-3xl md:text-5xl font-black text-transparent bg-clip-text bg-gradient-to-r from-[var(--color-primary-orange)] to-[var(--color-secondary-green)] mb-2 tracking-wide pt-10 pb-4 text-center">{{ card.title }}</h3>
+            <p class="text-zinc-300 md:py-10 text-center">
+              <span class="text-[var(--color-primary-orange)] text-2xl pb-2 md:text-3xl font-bold">
                 ${{ card.price.toLocaleString() }}
               </span> / {{ card.duration }}天
             </p>
-            <div v-for="(benefit, idx) in card.benefits" :key="idx" class="flex py-4">
+            <div v-for="(benefit, idx) in card.benefits" :key="idx" class="flex pt-6 md:py-4">
               <i class="fa-solid fa-check text-[var(--color-primary-orange)] pr-4"></i>
               <p class="text-zinc-200 font-bold">
                 {{ benefit.benefit.replace('1 次', `${benefit.counts} 次 `) }}
@@ -159,10 +159,10 @@ onUnmounted(() => {
               :disabled="isSubscribed(card.type)"
               type="button"
               :class="[
-                'mt-20 mb-10 px-6 py-2 text-lg rounded-[12px] block mx-auto transition',
+                'mt-8 mb-8 md:mt-20 md:mb-10 px-6 py-2 text-lg rounded-[12px] block mx-auto transition',
                 isSubscribed(card.type)
                   ? 'disabled:bg-[var(--color-primary-orange)] text-white text-2xl'
-                  : 'text-stone-200 border-2 border-[var(--color-primary-orange)] bg-neutral-900 hover:bg-[var(--color-primary-orange)]'
+                  : 'text-stone-200 border-2 border-[var(--color-primary-orange)] bg-neutral-900 active:bg-[var(--color-primary-orange)] md:hover:bg-[var(--color-primary-orange)]'
               ]"
             >
               {{ isSubscribed(card.type) ? '已訂閱' : '即刻擁有' }}
@@ -172,8 +172,8 @@ onUnmounted(() => {
         </div>
       </div>
     </div>
-    <div class="flex justify-center relative bottom-30">
-      <div class="w-120 h-1 bg-gradient-to-r from-transparent via-[#afb18c] through-[#e1ac67] to-transparent rounded-full shadow-lg">
+    <div class="flex justify-center relative mt-12 md:mt-8 md:bottom-30">
+      <div class="w-80 md:w-120 h-1 bg-gradient-to-r from-transparent via-[#afb18c] through-[#e1ac67] to-transparent rounded-full shadow-lg">
       </div>
     </div>
   </div>
